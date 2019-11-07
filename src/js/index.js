4c4e4249cf11c4e451caf29460591456b004b473
Johannes Tegnér <johannes@jitesoft.com>
2019-11-07 20:00:33 +0100

    Import of images via js.

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import 'img/favicon.ico';
+import 'img/favicon-96x96.png';
+import 'img/favicon-64x64.png';
+import 'img/favicon-32x32.png';
+import 'img/favicon-16x16.png';
+import 'img/og-image.png';
